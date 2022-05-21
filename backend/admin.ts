330dd9f1dff579917f1e5f1b34229b7acf7d8a5a
nicole <colebrazinski15@gmail.com>
2022-05-20 23:49:07 -0400

    add boilerplate for registering admin api endpoints

--- backend/admin.ts
+++ backend/admin.ts
@@ -1,6 +1,22 @@
 import { Turtle } from "./turtle";
 
+class Endpoint {
+  name: string;
+  func: () => string;
+
+  constructor(name: string, func: () => string) {
+    this.name = name;
+    this.func = func;
+  }
+}
+
 export class Admin {
+  public endpoints: Endpoint[] = [];
+
+  command(name: string, command: () => string) {
+    this.endpoints.push(new Endpoint(name, command));
+  }
+
   getSelectedSlot(turtle: Turtle) {
     return turtle.selectedSlot;
   }
